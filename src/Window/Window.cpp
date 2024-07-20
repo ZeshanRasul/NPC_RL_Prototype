d2e143d80a84360d1f14a0db40ce19804b16e8bd
Zeshan Rasul <zeshanrasul90@gmail.com>
2024-07-20 17:41:19 +0100

    Create Logger class and start Window class

---
+++ src/Window/Window.cpp
@@ -0,0 +1,14 @@
+#include "Window.h"
+
+bool Window::init(unsigned int width, unsigned int height, std::string title)
+{
+    return false;
+}
+
+void Window::mainLoop()
+{
+}
+
+void Window::cleanup()
+{
+}
