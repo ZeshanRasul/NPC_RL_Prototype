3d5328120c6515570055c9402e3020b255d18145
Zeshan Rasul <zeshanrasul90@gmail.com>
2024-07-23 19:06:11 +0100

    Directional Light calculations in fragment shader

--- src/Shaders/fragment.glsl
+++ src/Shaders/fragment.glsl
@@ -4,11 +4,39 @@ layout (location = 1) in vec2 texCoord;
 
 out vec4 FragColor;
 
+struct DirLight {
+	vec3 direction;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform DirLight dirLight;
+
+vec3 CalcDirLight(DirLight light, vec3 normal);
+
 uniform sampler2D tex;
-vec3 lightPos = vec3(4.0, 5.0, -3.0);
-vec3 lightColor = vec3(1.0, 1.0, 1.0);
+
 
 void main() {
-  float lightAngle = max(dot(normalize(normal), normalize(lightPos)), 0.0);
-  FragColor = texture(tex, texCoord) * vec4((0.3 + 0.7 * lightAngle) * lightColor, 1.0);
+	vec3 norm = normalize(normal);
+
+	vec3 result = CalcDirLight(dirLight, norm);
+
+	FragColor = vec4(result, 1.0);
+}
+
+vec3 CalcDirLight(DirLight light, vec3 normal)
+{
+	vec3 lightDir = normalize(-light.direction);
+
+	// diffuse
+	float diff = max(dot(normal, lightDir), 0.0);
+
+	// combine results
+	vec3 ambient = light.ambient * vec3(texture(tex, texCoord));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(tex, texCoord));
+
+	return (ambient + diffuse);
 }
