57fa70c7a97e8a331091887676c3cab703bb0608
Zeshan Rasul <zeshanrasul90@gmail.com>
2025-10-27 22:07:38 +0000

    Restart ECS Refactor

--- src/Game/Prefabs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "EnTT/entt.hpp"
-#include "Engine/Asset/AssetManager.h"
-#include "Engine/ECS/Components/StaticModelRendererComponent.h"
-#include "Engine/ECS/Components/TransformComponent.h"
-
-entt::entity CreateLevel(entt::registry& registry, AssetManager& assetManager,
-	const std::string& pathToGltf);
