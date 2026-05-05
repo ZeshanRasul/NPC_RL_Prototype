479175d79220bb14ed8065d443dbd29ab795ad95
Zeshan Rasul <zeshanrasul90@gmail.com>
2026-05-05 21:34:09 +0100

    Enemy textures correct

--- src/Shaders/pbr_fragment_emissive.glsl
+++ src/Shaders/pbr_fragment_emissive.glsl
@@ -178,9 +178,9 @@ void main()
     vec3 color = ambient + Lo + emissive;
 
     // HDR tonemapping
-    color = color / (color + vec3(1.0));
+//    color = color / (color + vec3(1.0));
     // gamma correct
-    color = pow(color, vec3(1.0/2.2)); 
+//    color = pow(color, vec3(1.0/2.2)); 
 
 //    float distanceToCamera = length(cameraPos - WorldPos);
 //
